6f55ddbb370b0f5c4c5f75ca8403b798adb2d0c7
Lachlan Alston <lachlan@alston.xyz>
2021-10-24 15:41:44 +1100

    Moved files

--- powerOn.py
+++
@@ -1,7 +0,0 @@
-import serial
-BAUD_RATE = 57600
-DEVICE = '/dev/ttyUSB0'
-s = serial.Serial(DEVICE, BAUD_RATE)
-s.write('<POWERON>>'.encode('utf-8'))
-s.close()
-exit()
--- reboot.py
+++
@@ -1,8 +0,0 @@
-import serial
-BAUD_RATE = 57600
-DEVICE = '/dev/ttyUSB0'
-s = serial.Serial(DEVICE, BAUD_RATE)
-print("SYSTEM REBOOTING")
-s.write('<REBOOT>>'.encode('utf-8'))
-s.close()
-exit()
